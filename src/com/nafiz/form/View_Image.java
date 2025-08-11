f1168e10de4b90a745f2d80b69218a0b20967aa0
Nafiz Kamal Talha <nafizkamaltalha2494@gmail.com>
2025-08-11 23:55:38 +0600

    Interface almost Done

---
+++ src/com/nafiz/form/View_Image.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
+ */
+package com.nafiz.form;
+
+import com.nafiz.event.PublicEvent;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import javax.swing.Icon;
+import javax.swing.SwingUtilities;
+
+public class View_Image extends javax.swing.JComponent {
+
+    public View_Image() {
+        initComponents();
+    }
+
+    private Icon image;
+
+    public void viewImage(Icon image) {
+        this.image = image;
+        pic.setImage(image);
+        setVisible(true);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        pic = new com.nafiz.swing.PictureBox();
+        cmdSave = new javax.swing.JButton();
+
+        pic.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                picMousePressed(evt);
+            }
+        });
+
+        cmdSave.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/nafiz/icon/save.png"))); // NOI18N
+        cmdSave.setContentAreaFilled(false);
+        cmdSave.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        cmdSave.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdSaveActionPerformed(evt);
+            }
+        });
+
+        pic.setLayer(cmdSave, javax.swing.JLayeredPane.DEFAULT_LAYER);
+
+        javax.swing.GroupLayout picLayout = new javax.swing.GroupLayout(pic);
+        pic.setLayout(picLayout);
+        picLayout.setHorizontalGroup(
+            picLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, picLayout.createSequentialGroup()
+                .addContainerGap(704, Short.MAX_VALUE)
+                .addComponent(cmdSave, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        picLayout.setVerticalGroup(
+            picLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, picLayout.createSequentialGroup()
+                .addContainerGap(462, Short.MAX_VALUE)
+                .addComponent(cmdSave)
+                .addContainerGap())
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(pic, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(pic, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void picMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_picMousePressed
+        if (SwingUtilities.isLeftMouseButton(evt)) {
+            setVisible(false);
+        }
+    }//GEN-LAST:event_picMousePressed
+
+    private void cmdSaveActionPerformed(ActionEvent evt) {//GEN-FIRST:event_cmdSaveActionPerformed
+        PublicEvent.getInstance().getEventImageView().saveImage(image);
+    }//GEN-LAST:event_cmdSaveActionPerformed
+
+    @Override
+    protected void paintComponent(Graphics grphcs) {
+        Graphics2D g2 = (Graphics2D) grphcs;
+        g2.setColor(new Color(0, 0, 0, 150));
+        g2.fillRect(0, 0, getWidth(), getHeight());
+        super.paintComponent(grphcs);
+    }
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton cmdSave;
+    private com.nafiz.swing.PictureBox pic;
+    // End of variables declaration//GEN-END:variables
+}
